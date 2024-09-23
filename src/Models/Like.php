150592a3345fc07e8231a480fc5d4267151d863f
Tan Nguyen <tannp27@gmail.com>
2024-09-24 02:39:32 +0700

    feat: update like model

--- src/Models/Like.php
+++ src/Models/Like.php
@@ -2,6 +2,20 @@
 
 namespace CSlant\LaravelLike\Models;
 
-class Like
+use CSlant\LaravelLike\Enums\LikeTypeEnum;
+use Illuminate\Database\Eloquent\Model;
+
+class Like extends Model
 {
+    protected $fillable = [
+        'user_id',
+        'model_id',
+        'model_type',
+        'type'
+    ];
+
+    protected $casts = [
+        'model_type' => 'string',
+        'type' => LikeTypeEnum::class
+    ];
 }
