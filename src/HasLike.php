b3c57f243c004b3720a765385d07171d7d93a329
Tan Nguyen <tannp27@gmail.com>
2024-09-24 13:14:32 +0700

    feat: add dislike for HasLike trait

--- src/HasLike.php
+++ src/HasLike.php
@@ -22,17 +22,37 @@ trait HasLike
      */
     public function likeOne(): MorphOne
     {
-        return $this->morphOne(Like::class, 'model');
+        return $this->morphOne(Like::class, 'model')->where('type', LikeTypeEnum::LIKE);
     }
 
     /**
-     * Like has many relationship with the model.
+     * Like has many relationship with the model for likes.
      *
      * @return MorphMany
      */
     public function likes(): MorphMany
     {
-        return $this->morphMany(Like::class, 'model');
+        return $this->morphMany(Like::class, 'model')->where('type', LikeTypeEnum::LIKE);
+    }
+
+    /**
+     * Like has one relationship with the model for dislikes.
+     *
+     * @return MorphOne
+     */
+    public function dislikeOne(): MorphOne
+    {
+        return $this->morphOne(Like::class, 'model')->where('type', LikeTypeEnum::DISLIKE);
+    }
+
+    /**
+     * Like has many relationship with the model for dislikes.
+     *
+     * @return MorphMany
+     */
+    public function dislikes(): MorphMany
+    {
+        return $this->morphMany(Like::class, 'model')->where('type', LikeTypeEnum::DISLIKE);
     }
 
     /**
