cdda60a93a25c97ae4d456c0ba7a800dcb55198d
tanhongit <tanhongit@users.noreply.github.com>
2024-09-25 15:55:09 +0000

    Fix styling

--- src/HasLike.php
+++ src/HasLike.php
@@ -2,9 +2,9 @@
 
 namespace CSlant\LaravelLike;
 
+use CSlant\LaravelLike\Traits\InteractionRelationship;
 use CSlant\LaravelLike\Traits\Like\LikeCount;
 use CSlant\LaravelLike\Traits\Like\LikeScopes;
-use CSlant\LaravelLike\Traits\InteractionRelationship;
 use Illuminate\Database\Eloquent\Model;
 
 /**
