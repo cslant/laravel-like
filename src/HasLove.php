13c4b6670709825f2409f1f47e3aca54f08a2d3c
Tan Nguyen <tannp27@gmail.com>
2024-09-27 00:57:51 +0700

    remove redundant eloquent relation in hasLove trait

--- src/HasLove.php
+++ src/HasLove.php
@@ -6,17 +6,12 @@ use CSlant\LaravelLike\Traits\InteractionRelationship;
 use CSlant\LaravelLike\Traits\Love\LoveCount;
 use CSlant\LaravelLike\Traits\Love\LoveScope;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\MorphMany;
-use Illuminate\Database\Eloquent\Relations\MorphOne;
 
 /**
  * Trait HasLove
  *
  * @package CSlant\LaravelLike
  * @mixin Model
- *
- * @method MorphOne likeOne()
- * @method MorphMany likes()
  */
 trait HasLove
 {
