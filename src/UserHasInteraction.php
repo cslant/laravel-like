aab741ee974ae426ba786cd154cddb6029a99ca5
Tan Nguyen <tannp27@gmail.com>
2024-10-06 18:13:46 +0700

    fix: update default interaction model

--- src/UserHasInteraction.php
+++ src/UserHasInteraction.php
@@ -3,6 +3,7 @@
 namespace CSlant\LaravelLike;
 
 use CSlant\LaravelLike\Enums\InteractionTypeEnum;
+use CSlant\LaravelLike\Models\Like;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
@@ -23,7 +24,7 @@ trait UserHasInteraction
      */
     public function likes(): HasMany
     {
-        $interactionModel = (string) (config('like.interaction_model') ?? 'CSlant\LaravelLike\Models\Like');
+        $interactionModel = (string) (config('like.interaction_model') ?? Like::class);
         $userForeignKey = (string) (config('like.users.foreign_key') ?? 'user_id');
 
         return $this->hasMany($interactionModel, $userForeignKey);
